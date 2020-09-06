8631d22410e461e4aae2ecdd4e563433c2adb7bf
Nikita Pashin <nikita-pashin12@yandex.ru>
2020-09-06 23:41:50 +0500

    Подключен redux (Provider)

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
 
-import './scss/app.scss'
+import './scss/app.scss';
+import store from './redux/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
