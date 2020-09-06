021987cc16f372c661f0431bf54c9a85643aeaef
Nikita Pashin <nikita-pashin12@yandex.ru>
2020-09-06 19:09:09 +0500

    Созданы новые компоненты: - Cards - Card

---
+++ src/components/Card/CardBlock.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CardBlock() {
+    return (
+        <div className="card">
+            <img src="https://img3.goodfon.ru/original/1280x1024/f/a4/interer-stil-dizayn-dom-6229.jpg" alt="Квартира"/>
+            <div className="card__inner"></div>
+        </div>
+    )
+}
+
+export default CardBlock
---
+++ src/components/Cards.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Card } from './index'
+
+function Cards() {
+    return (
+        <div className="container">
+            <div className="cards">
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </div>
+    )
+}
+
+export default Cards
