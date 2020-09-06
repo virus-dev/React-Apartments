97799eecbff972a54d02fed7897737f434f62c47
Nikita Pashin <nikita-pashin12@yandex.ru>
2020-09-06 18:00:15 +0500

    Первая версия header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import logo from './img/logo.svg'
 
 function App() {
   return (
-    <div></div>
+    <div className="Page">
+      <header>
+        <div className="container">
+          <div className="header__inner">
+            <div className="header__logo">
+              <img src={logo}></img>
+              <div className="header__logo__text"> 
+                <div>React</div> 
+                <div>Apartments</div> 
+              </div>
+            </div>  
+          </div>  
+        </div>
+      </header>
+    </div>
   );
 }
 
