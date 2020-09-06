e322365f0b3360bba2cd6cdac2e76b521371acf8
Nikita Pashin <nikita-pashin12@yandex.ru>
2020-09-06 18:07:26 +0500

    В App.js добавил компоненту Header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import logo from './img/logo.svg'
+import { Header } from './components/index'
 
 function App() {
   return (
     <div className="Page">
-      <header>
-        <div className="container">
-          <div className="header__inner">
-            <div className="header__logo">
-              <img src={logo}></img>
-              <div className="header__logo__text"> 
-                <div>React</div> 
-                <div>Apartments</div> 
-              </div>
-            </div>  
-          </div>  
-        </div>
-      </header>
+      <Header />
     </div>
   );
 }
