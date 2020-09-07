e3448c29ff86d8a62ded1aed96057ab4fda0a0b1
Nikita Pashin <nikita-pashin12@yandex.ru>
2020-09-07 09:12:27 +0500

    - Сделал компонент Cards - Добавил стили - Адаптировал

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import { Card } from './index'
+import { useSelector, useDispatch } from 'react-redux'
+import { fethApartments } from '../redux/actions/apartments'
+import MyLoader from './Card/CardLoading'
 
 function Cards() {
+    const state = useSelector(({ apartments }) => apartments);
+    const dispatch = useDispatch();
+
+    React.useEffect(() => {
+        dispatch(fethApartments())
+    }, []);
+
     return (
         <div className="container">
             <div className="cards">
-                <Card />
-                <Card />
-                <Card />
+                {
+                    state.isLoaded 
+                        ? state.apartments.map((obj, index) => {return <Card key={`${obj.name + index}`} apartment={obj} />})
+                        : Array(12).fill(0).map((_, index) => <MyLoader key={index} />)
+                }
             </div>
         </div>
     )
